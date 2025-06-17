2c22a7a3977086b3e0a0fd14732836c27d57ffbf
Andrew Plotkin <zarf@ZarfLent.local>
2025-06-16 22:59:13 -0400

    Move types around so that we can compile without Glk.

--- glkautosave.h
+++ glkautosave.h
@@ -1,14 +1,8 @@
 #ifndef ZTERP_GLKAUTOSAVE_H
 #define ZTERP_GLKAUTOSAVE_H
 
-enum class StreamRock : glui32 {
-    None = 0,
-    BlorbStream = 1,
-    TranscriptStream = 2,
-};
-
-/* Currently this header just declares the StreamRock enum. I'll add
-   some function declarations in a later patch. */
+/* Currently this header is empty. I'll add some function declarations
+   in a later patch. */
 
 #endif
 
